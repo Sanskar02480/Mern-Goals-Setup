144e9f61846b35356e7590555e18a0e4a6861af3
Sanskar02480 <sanskartolumbia55@gmail.com>
2026-01-29 22:33:28 +0530

    User Controller initiated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,62 @@
-const registerUser =(req,res) =>{
-    res.status(200).json({message:'Register User'})
-  }
+const jwt =require('jsonwebtoken')
+const bcrpyt =require('bcryptjs')
+const asyncHandler =require('express-async-handler')
+const User =require('../model/userModel')
+const userModel = require('../model/userModel')
+
+
+const registerUser =asyncHandler(async(req,res) =>{
+    const {name , email , password}=req.body
+
+    if(!name || !email || !password)
+    { 
+        res.status(400)
+        throw new Error('Please fill all the fields')
+    }
+
+    const UserExists =await userModel.findOne({email})
+    if(UserExists)
+    {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const salt =await bcrpyt.genSalt(10)
+    const hashedPassword =await bcrpyt.hash(password , salt)
+
+    const user =await User.create({
+        name ,
+        email ,
+        password :hashedPassword
+    })  
+
+    if(user)
+    {
+        res.status(201).json({
+        id: user.id,
+        name :user.name,
+        email :user.email
+    })
+    }
+    else
+    {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+    
+})
+
+
+const LoginUser =asyncHandler(async(req,res) =>{
+    res.status(200).json({message:'Login User'})
+})
+
+const ShowUser =asyncHandler(async  (req,res) =>{
+    res.status(200).json({message:'Show User'})
+})
+
+module.exports={
+    registerUser,
+    LoginUser,
+    ShowUser
+}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,15 @@
+
+
 const express =require('express')
 const router =express.Router()
 
-const{registerUser} =require('../controllers/userController')
+const{registerUser ,LoginUser ,ShowUser} =require('../controllers/userController')
 
 
 router.post('/',registerUser)
 
+router.get('/login',LoginUser)
 
-mosule .exports =router
+router.get('/Profile',ShowUser)
+
+module.exports =router
